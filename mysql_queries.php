30a50e2189f2bc3cd51c03f0157dd87757b845ae
Marek <marekm2211@gmail.com>
2015-10-23 11:59:33 +0200

    MySQL queries added

--- mysql_queries.php
+++ mysql_queries.php
@@ -7,10 +7,27 @@ stock INT,
 PRIMARY KEY (product_id));
 
 
-CREATE TABLE image(
+CREATE TABLE images(
 image_id INT AUTO_INCREMENT,
 product_id INT,
 link VARCHAR(100),
 PRIMARY KEY (image_id),
 FOREIGN KEY (product_id) REFERENCES products (product_id)
+);
+
+CREATE TABLE users(
+user_id INT AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(50),
+email VARCHAR(50),
+password VARCHAR(32),
+address VARCHAR(50),
+PRIMARY KEY (user_id)
+);
+
+CREATE TABLE admins(
+admin_id INT AUTO_INCREMENT,
+email VARCHAR(50),
+password VARCHAR(32),
+PRIMARY KEY (admin_id)
 );
